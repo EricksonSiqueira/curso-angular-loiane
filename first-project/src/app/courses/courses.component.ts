b32f6f37695da0f587e2ae629dc9b1eea4fe206c
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-11-22 17:51:49 -0300

    feat: learning template and directives

--- first-project/src/app/courses/courses.component.ts
+++ first-project/src/app/courses/courses.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
+  portalName: string;
 
+  courses: string[] = ['Java', 'Ext JS', 'AngularJS'];
+
+  constructor() {
+    this.portalName = 'https://loiane.training/';
+  }
 }
