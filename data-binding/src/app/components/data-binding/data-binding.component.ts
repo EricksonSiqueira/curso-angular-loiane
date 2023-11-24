c455fa862851a451dba6231ef87a70aff1e5d516
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-11-24 14:18:55 -0300

    feat: event binding

--- data-binding/src/app/components/data-binding/data-binding.component.ts
+++ data-binding/src/app/components/data-binding/data-binding.component.ts
@@ -10,11 +10,31 @@ export class DataBindingComponent {
   angularCourse = true;
   imageUrl = 'https://picsum.photos/200/300';
 
+  actualInputValue = '';
+  savedValue = '';
+  isMouseOver = false;
+
   getValue() {
     return 1;
   }
 
   getLikeCourse() {
     return true;
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.actualInputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  saveValue(value: string) {
+    this.savedValue = value;
+  }
+
+  buttonClicked() {
+    return alert('Button clicked!');
+  }
+
+  onMouseOverAndOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 }
