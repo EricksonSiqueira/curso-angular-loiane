c1c13ce8dd8b1475b0cbd7c56c22265c6280c3db
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-11-28 17:39:19 -0300

    feat: put angular in pt-br

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PipesExamplesComponent } from './pipes-examples/pipes-examples.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { SettingsService } from './settings.service';
+
+registerLocaleData(localePt);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PipesExamplesComponent,
-    CamelCasePipe
+  declarations: [AppComponent, PipesExamplesComponent, CamelCasePipe],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService: SettingsService) =>
+        settingsService.getLocale(),
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
