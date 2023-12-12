1b3ebc765ca895a6b3b1a875313813d06d430f13
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-12-12 01:04:42 -0300

    feat: create service and environment

--- http-requests/src/app/courses/courses.service.ts
+++ http-requests/src/app/courses/courses.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ICourse } from './interfaces/course';
+import { environment } from 'src/environments/environments';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,7 @@ import { ICourse } from './interfaces/course';
 export class CoursesService {
   constructor(private http: HttpClient) {}
 
-  private readonly API_URL = 'http://localhost:3000/courses';
+  private readonly API_URL = `${environment.API}courses`;
 
   list() {
     return this.http.get<ICourse[]>(this.API_URL);
