24bd8833f6ffdd229c3c400c565d125852839800
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-12-12 01:11:35 -0300

    feat: creating observable and pipe async

--- http-requests/src/app/courses/courses-list/courses-list.component.ts
+++ http-requests/src/app/courses/courses-list/courses-list.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ICourse } from '../interfaces/course';
 import { CoursesService } from '../courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.scss'],
 })
-export class CoursesListComponent implements OnInit {
-  courses!: ICourse[];
+export class CoursesListComponent implements OnInit, OnDestroy {
+  courses$!: Observable<ICourse[]>;
 
   constructor(private coursesService: CoursesService) {}
 
   ngOnInit(): void {
-    this.coursesService.list().subscribe((courses) => (this.courses = courses));
+    this.courses$ = this.coursesService.list();
   }
+
+  ngOnDestroy(): void {}
 }
